9a5bf174ce5bd0c4048e03c4cc8b018eed1aba16
harshitbajpai85@gmail.com <harshitbajpai85@gmail.com>
2020-05-02 14:43:55 +0530

    changes

---
+++ data/profile.js
@@ -0,0 +1,167 @@
+    const profileData = {
+        "title": "Profile",
+        "name": "Harshit Bajpai",
+        "sub_title": "B. Tech undergraduate (Computer Science and Engineering)",
+        "about": {
+            "intro": "Extremely motivated to constantly develop my skills and grow professionally. I am confident in my ability to come up with a solution for every problem.I am always eager to learn from anyone and everyone. I am a strong team player who is able to quickly learn and apply new technologies with 1+ year of experience",
+            "contact": {
+                 "email": "harshitbajpai85@gmail.com",
+                "phone": "+919696831239",
+                "address": "562, T.R Muniswamappa Building,1st Main 5th cross ,BENGALURU ,KARNATAKA-560066",
+                "portfolio": "https://github.com/harshit85"
+            }
+        },
+        "links": [
+            { "title": "GitHub", "src": "https://github.com/harshit85" },
+            { "title": "HackerRank", "src": "https://www.hackerearth.com/@harshitbajpai85" },  
+        { "title": "Twitter", "src": "https://twitter.com/harshitbajpai85" },  
+            { "title": "LinkedIn", "src": "https://www.linkedin.com/in/harshit-bajpai-a239ab10b/" }
+        ],
+        "education": [
+            {
+                "alma": "Faculty of Engineering and Technology, GKV, Haridwar",
+                "duration": "2014 - 2018", "std": "B. Tech (CSE)",
+                "score": "73.33%"
+            },
+            {
+                "alma": "St francis Xavier's Inter College,Ashok Nagar Kanpur, Uttar Pradesh",
+                "duration": "2011 - 2013", "std": "Class XI - XII",
+                "score": "90%"
+            },
+            {
+                "alma": "St francis Xavier's HSS, Ashok Nagar Kanpur, Uttar Pradesh",
+                "duration": "2006 - 2011", "std": "Class X",
+                "score": "76.16%"
+            }
+        ],
+        "skills": [
+            {
+                "graph": "false",
+                "category": "Programming Languages",
+                "topics": [
+                    "C","C++","Java","C#","Python"
+                ]
+            },
+            {
+                "graph": "false",
+                "category": "Scripting Languages",
+                "topics": [
+                    "HTML","CSS","Java Script","Jqery"
+                ]
+            },
+        {
+                "graph": "false",
+                "category": "Databases",
+                "topics": [
+                    "MySQL","SQLite","Room"
+                ]
+            },
+            {
+                "graph": "false",
+                "category": "libraries",
+                "topics": [
+                    "Dagger","Retrofit2","RxJava",
+                ]
+            },
+            {
+                "graph": "false",
+                "category": "Technologies/Frameworks",
+                "topics": [
+                    "Android",".net",
+                ]
+            },
+            {
+                "graph": "false",
+                "category": "Backend",
+                "topics": [
+                    "PHP"
+                ]
+            }
+        ],
+        "projects": [{
+                    "title": "Microid",
+            "duration": "February, 2020 currently",
+                    "link": "https://play.google.com/store/apps/details?id=com.mantis.blackoasis",
+                    "desc": "Creating multiple android apps with single code base by changing product flavour"
+                }
+                ,{
+                    "title": "GitSearch",
+            "duration":"January, 2020",
+                    "link": "https://github.com/harshit85/gitSearchApp/",
+                    "desc": "It's an application to search directories related to different technologies, different user's directories"
+                }
+            ,{
+                    "title": "CRS (Computerized Reservations System)",
+            "duration":"July 2019 - January 2020",
+                    "link": "",
+                    "desc": "It's an B2B desktop application currently used by 3000 different bus operator all over the country for bus ticket booking and Cargo booking. It's also used for analysising sales and booking. With this application operators can add agents in their companies for enhance booking.<br><b>Technologies used: - C#, Asp.Net, Ado.Net, WinForms, MySQL.</b>" 
+                },{
+                    "title": "BusCrs",
+            "duration":"April ,2020 - presently working",
+                    "link": "https://http://buscrs.com/",
+                    "desc": "An website version of CRS Des application with less functionalities"
+                },{
+                    "title": "bookBusTicket",
+            "duration":"January, 2019 - till today",
+                    "link": "https://http://bookbustickets.com/",
+                    "desc": "It's an bus ticket booking platform for the agents where agents can book ticket in different routes"
+                }
+
+        ],
+        "experiences": [
+        {
+                "desc": "<b> Android Developer </b> at <a target='_blank' href='http://www.mantistechnologies.com/'> <strong> Mantis Technologies </strong> </a>",
+                "date": "Faburary 01,2020 (presently)",
+                "details": [
+                   "<b>Redeveloping MICROID written in java</b> - It's a project for creating multiple android application with single codebase. It provides bus ticket booking android applications for every bus operator. It's a B2C mobile application which uses by the customer to do booking.",
+                    "using libraries like RxJava,Retrofit2,Dagger,AutoValue,Icepick and manymore popular libraries"
+                ]
+            },
+        {
+                "desc": "<b>Software Engineer </b> at  <a target='_blank' href='https://www.mantistechnologies.com'> <strong> Mantis Technologies </strong> </a>",
+                "date": "july 2019 - January 2020",
+                "details": [
+                    "<b>Worked on company's core products (CRS ,busCRS, bookBusTicket ,IndoCanadianCRS)</b>",
+                    "Created many company level configurations like- Hide Booking, Hide Inquiery Reports",
+                    "Created coupon System which is being used by the different bus operators.",
+                    "Created voucher System for IndoCanadianCRS",
+                    "Created many Api's for our internal project and also for third party application and mobile applications.",
+                    "Done too many enhancements in many different functionalities"
+                ]
+            }
+        ],
+        "events": [
+            {
+                "title": "certifications",
+                "body": [
+                    {
+                        "desc": "a three months course in<strong> Core Java </strong> from <b> S.K.Infotech,Haridwar </b>",
+                        "date": "September - November, 2017"
+                    },
+                    {
+                        "desc": "a training program in <strong> Python </strong> conducted by <b>IIT Bombay</b>funded by<b>MHRD,India Government</b>",
+                        "date": "December, 2017"
+                    }
+                ]
+            },
+            {
+                "title": "other",
+                "body": [
+                    
+                    
+                    {
+                         "desc": "Secured <strong> Rank 5777</strong> in <b> Gate CS 2019</b> with <b> Score 496</b>",
+                         "date": "March 2019"
+                    },
+                    {
+                         "desc":"Represent our district in state conference of National Children Science Congress",
+                        "date":"January 2011"
+                    },
+                    {
+                         "desc":"got an scholarship from <b>National Children Science Congress</b>",
+                        "date": "December 2010"
+                    }
+                ]
+            }
+        ]
+    }
